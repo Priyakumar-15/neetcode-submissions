cb3c3938e088f620099d71448c101f05c6d018b0
Priya kumar <146212812+Priyakumar-15@users.noreply.github.com>
2026-06-22 22:49:12 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+      if(s.length() != t.length()){
+        return false;
+      }
+         int freq[]=new int[256];
+         for(char ch:s.toCharArray()){
+          freq[ch]++;
+         }
+         for(char ch:t.toCharArray()){
+          freq[ch]--;
+         }
+         for(int i:freq){
+          if(i != 0) return false;
+         }
+         return true;
+    }
+}
