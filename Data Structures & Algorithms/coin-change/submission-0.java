c3bd11e6de0e27511f64688f6e8a08ef55d5641c
Priya kumar <146212812+Priyakumar-15@users.noreply.github.com>
2026-07-23 13:53:22 +0530

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+           int n = coins.length;
+         int[][] dp = new int[n+1][amount+1];
+        for(int i=0;i<n+1;i++){
+           
+                     dp[i][0]=0;
+        }
+        for(int j=1;j<amount+1;j++){
+           
+                     dp[0][j]=Integer.MAX_VALUE-1;
+        }
+        for(int j=1;j<amount+1;j++){
+            if(j % coins[0] == 0){
+                dp[1][j]=j/coins[0];
+            }else{
+                 dp[1][j]=Integer.MAX_VALUE-1;
+            }
+           
+                    
+        }
+                 
+                 
+                 for(int i = 1;i<n+1;i++){
+                    for(int j = 1 ;j< amount+1;j++){
+                        if(coins[i-1]<=j){
+                            dp[i][j]= Math.min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
+                        }else{
+                        dp[i][j]=dp[i-1][j];
+                        }
+                    }
+                 }
+                 return dp[n][amount] == Integer.MAX_VALUE-1 ? -1:dp[n][amount];
+    }
+}
